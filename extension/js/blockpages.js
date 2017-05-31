94502ce99b3b73f4c3fabe5546282a38e1d4c3f1
Benjamin Sherman <benjaminrsherman@gmail.com>
2017-05-30 23:44:02 -0400

    Updated special website functionality

--- extension/js/blockpages.js
+++ extension/js/blockpages.js
@@ -1,40 +1,19 @@
-// Brought in from special_websites.json
-data = {
-	"allowed": ["bing.com",
-	"yahoo.com",
-	"hulu.com",
-	"vimeo.com",
-	"alibaba.com",
-	"myspace.com",
-	"tidal.com",
-	"microsoft.com",
-	"christianmingle.com"],
+$.getJSON("https://raw.githubusercontent.com/benjaminrsherman/no-nn-simulator/master/special_websites.json", function(data) {
+	var url = window.location.href;
 
-	"blocked": [{ "name": ["mail.google.com"], "alternateurl": "mail.yahoo.com", "alternatename": "Yahoo! Mail" },
-	{ "name": ["pandora.com", "spotify.com", "music.google.com", "music.amazon.com"], "alternateurl": "tidal.com", "alternatename": "Tidal" },
-	{ "name": ["google.com"],	"alternateurl": "bing.com", "alternatename": "Bing" },
-	{ "name": ["netflix.com"], "alternateurl": "hulu.com", "alternatename": "Hulu" },
-	{ "name": ["youtube.com"], "alternateurl": "vimeo.com", "alternatename": "Vimeo" },
-	{ "name": ["amazon.com", "ebay.com"], "alternateurl": "alibaba.com", "alternatename": "Alibaba" },
-	{ "name": ["facebook.com", "twitter.com", "reddit.com", "tumblr.com"], "alternateurl": "myspace.com", "alternatename": "Myspace" },
-	{ "name": ["apple.com"], "alternateurl": "microsoft.com", "alternatename": "Microsoft" },
-	{ "name": ["pornhub.com", "youporn.com", "xvideos.com", "redtube.com", "tube8.com", "brazzers.com", "pornmd.com", "thumbzilla.com", "realitykings.com", "mydirtyhobby.com", "seancody.com", "men.com", "digitalplayground.com", "mofos.com", "babes.com", "gaytube.com", "twistys.com", "peeperz.com", "sextube.com", "porniq.com", "webcams.com"], "alternateurl": "christianmingle.com", "alternatename": "Christian Mingle"}]
-}
-
-var url = window.location.href;
-
-loop:
-	for (b in data["blocked"]) {
-		var replace = false;
-		var block = data["blocked"][b];
-		for (u in block["name"]) {
-			var URL = block["name"][u];
-			if (url.indexOf(URL) !== -1) {
-				replace = true;
-				window.alert("This website does not function correctly on your ISP's service, please use " + block["alternatename"] + " instead.");
-				break loop;
+	loop:
+		for (b in data["blocked"]) {
+			var replace = false;
+			var block = data["blocked"][b];
+			for (u in block["name"]) {
+				var URL = block["name"][u];
+				if (url.indexOf(URL) !== -1) {
+					replace = true;
+					window.alert("This website does not function correctly on your ISP's service, please use " + block["alternatename"] + " instead.");
+					break loop;
+				}
 			}
 		}
-	}
 
-$(document.body).append("<div class='nnn-header'><h2>Your internet is being throttled, courtesy of your ISP!</h2></div>");
+	$(document.body).append("<div class='nnn-header'><h2>Your internet is being throttled, courtesy of your ISP!</h2></div>");
+});
--- extension/js/throttler.js
+++ extension/js/throttler.js
@@ -1,59 +1,42 @@
-// Brought in from special_websites.json
-data = {
-	"allowed": ["bing.com",
-	"yahoo.com",
-	"hulu.com",
-	"vimeo.com",
-	"alibaba.com",
-	"myspace.com",
-	"tidal.com",
-	"microsoft.com",
-	"christianmingle.com"],
-
-	"blocked": [{ "name": ["mail.google.com"], "alternateurl": "mail.yahoo.com", "alternatename": "Yahoo! Mail" },
-	{ "name": ["pandora.com", "spotify.com", "music.google.com", "music.amazon.com"], "alternateurl": "tidal.com", "alternatename": "Tidal" },
-	{ "name": ["google.com"],	"alternateurl": "bing.com", "alternatename": "Bing" },
-	{ "name": ["netflix.com"], "alternateurl": "hulu.com", "alternatename": "Hulu" },
-	{ "name": ["youtube.com"], "alternateurl": "vimeo.com", "alternatename": "Vimeo" },
-	{ "name": ["amazon.com", "ebay.com"], "alternateurl": "alibaba.com", "alternatename": "Alibaba" },
-	{ "name": ["facebook.com", "twitter.com", "reddit.com", "tumblr.com"], "alternateurl": "myspace.com", "alternatename": "Myspace" },
-	{ "name": ["apple.com"], "alternateurl": "microsoft.com", "alternatename": "Microsoft" },
-	{ "name": ["pornhub.com", "youporn.com", "xvideos.com", "redtube.com", "tube8.com", "brazzers.com", "pornmd.com", "thumbzilla.com", "realitykings.com", "mydirtyhobby.com", "seancody.com", "men.com", "digitalplayground.com", "mofos.com", "babes.com", "gaytube.com", "twistys.com", "peeperz.com", "sextube.com", "porniq.com", "webcams.com"], "alternateurl": "christianmingle.com", "alternatename": "Christian Mingle"}]
-}
-
 var attachedTabs = {};
 var version = "1.0";
 var allowedURLs = [];
 
-for (b in data["blocked"]) {
-	var replace = false;
-	var block = data["blocked"][b];
-	for (u in block["name"]) {
-		var URL = "*://www." + block["name"][u] + "/*";
-		chrome.contentSettings.javascript.set(
-			{
-				primaryPattern: URL,
-				setting: "block"
-			}
-		);
-		chrome.contentSettings.plugins.set(
-			{
-				primaryPattern: URL,
-				setting: "block"
-			}
-		);
-		chrome.contentSettings.images.set(
-			{
-				primaryPattern: URL,
-				secondaryPattern: "<all_urls>",
-				setting: "block"
-			}
-		);
-	}
-}
+var data;
 
-$(document).ready(function() {
-	allowedURLs = data["allowed"];
+$.getJSON("http://raw.githubusercontent.com/benjaminrsherman/no-nn-simulator/master/special_websites.json", function(ret) {
+	data = ret;
+
+	for (b in data["blocked"]) {
+		var replace = false;
+		var block = data["blocked"][b];
+		for (u in block["name"]) {
+			var URL = "*://www." + block["name"][u] + "/*";
+			chrome.contentSettings.javascript.set(
+				{
+					primaryPattern: URL,
+					setting: "block"
+				}
+			);
+			chrome.contentSettings.plugins.set(
+				{
+					primaryPattern: URL,
+					setting: "block"
+				}
+			);
+			chrome.contentSettings.images.set(
+				{
+					primaryPattern: URL,
+					secondaryPattern: "<all_urls>",
+					setting: "block"
+				}
+			);
+		}
+	}
+
+	$(document).ready(function() {
+		allowedURLs = data["allowed"];
+	});
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
@@ -88,6 +71,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 });
 
 function onAttach(debuggeeId) {
+	data = $.getJSON("https://raw.githubusercontent.com/benjaminrsherman/no-nn-simulator/master/special_websites.json");
 	var tabId = debuggeeId.tabId;
 	attachedTabs[tabId] = true;
 	chrome.debugger.sendCommand(debuggeeId, "Network.enable", null, function() {
